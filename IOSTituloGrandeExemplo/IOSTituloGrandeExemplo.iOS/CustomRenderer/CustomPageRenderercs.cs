40dac14bb1aec97a3d276b7cbccfe5ed4b584524
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-04-10 11:09:57 -0300

    Primeira versão do Exemplo

---
+++ IOSTituloGrandeExemplo/IOSTituloGrandeExemplo.iOS/CustomRenderer/CustomPageRenderercs.cs
@@ -0,0 +1,35 @@
+﻿using IOSTituloGrandeExemplo.Controles;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace IOSTituloGrandeExemplo.iOS
+{
+    public class CustomPageRenderer : PageRenderer
+    {
+        //Quando a View Aparecer verifica a propriedade e marca as preferencias para titulos grandes
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            var exibirTituloGrande = TituloGrande.GetExibirTituloGrande(Element);
+
+            //Apenas Devices com IOS 11
+            if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0) && NavigationController != null && NavigationController.NavigationBar != null)
+            {
+                NavigationController.NavigationBar.PrefersLargeTitles = exibirTituloGrande;
+            }
+        }
+
+        //Quando a View Desaparecer marca as preferencias para não exibir titulos grandes
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+
+            //Apenas Devices com IOS 11
+            if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0) && NavigationController != null && NavigationController.NavigationBar != null)
+            {
+                NavigationController.NavigationBar.PrefersLargeTitles = false;
+            }
+        }
+    }
+}
---
+++ IOSTituloGrandeExemplo/IOSTituloGrandeExemplo/Controles/TituloGrande.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace IOSTituloGrandeExemplo.Controles
+{
+    public class TituloGrande
+    {
+        public static readonly BindableProperty ExibirTituloGrandeProperty =
+            BindableProperty.CreateAttached("ExibirTituloGrande", typeof(bool), typeof(TituloGrande), false);
+
+        public static bool GetExibirTituloGrande(BindableObject view)
+        {
+            return (bool)view.GetValue(ExibirTituloGrandeProperty);
+        }
+
+        public static void SetExibirTituloGrande(BindableObject view, bool value)
+        {
+            view.SetValue(ExibirTituloGrandeProperty, value);
+        }
+    }
+}
